7af9ea58efc7cdec119c06f4c45200aac32cf15f
Anton Scotte <ascotte@kth.se>
2022-02-24 15:17:28 +0100

    Optimized with cython and numpy

---
+++ Assignment 3/cythonfn_numpy.pyx
@@ -0,0 +1,15 @@
+#cython: boundscheck=False
+# cythonfn_new.pyx
+import numpy as np
+cimport numpy as np
+def gauss_seidel(double[:, :] f):
+	cdef unsigned int i, j
+	cdef double[:, :] newf
+	newf = f.copy()
+    
+	for i in range(1,newf.shape[0]-1):
+		for j in range(1,newf.shape[1]-1):
+			newf[i,j] = 0.25 * (newf[i,j+1] + newf[i,j-1] +
+								   newf[i+1,j] + newf[i-1,j])
+
+	return newf
